7d27d00aab86f89ab8f35fbb9b0569d57f65690d
guoqirong <2834964850@qq.com>
2022-05-20 18:25:05 +0800

    批量读取文件夹文件

--- bin/json-to-excel.js
+++ bin/json-to-excel.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const commander = require('commander');
-const { excel2json } = require('../lib/excel-to-json');
+const { json2excel } = require('../lib/json-to-excel');
 const package = require('../package.json');
 
 const program = new commander.Command();
@@ -13,10 +13,8 @@ program
 
 // 自定义选项参数
 program
-  .option('-i, --input <file>', '必填，输入要导成json的xls、xlsx文件')
-  .option('-r, --rule <key-col-num:key-col-interval>', '选填，解析表格规制，key-col-num为语言包key列索引，key-col-interval为语言包数据范围（如：0:[4,15]表示第一列为key，第五列到第十九列为语言包数据），默认为0:[1]', '0:[1]')
-  .option('-a, --add', '选填，增量导出数据，先获取原有数据')
-  .option('-o, --output <path>', '选填，输出文件路径及后缀配置，文件名以**代替，文件存储路径必须存在，默认为./**.ts', './**.ts')
+  .option('-i, --input <path>', '必填，输入文件路径及后缀配置，文件名以**代替，文件支持ts、js和json，如：./**.ts')
+  .option('-o, --output <file>', '选填，输出要导成的xls、xlsx文件，可带路径，默认为local.xls', 'local.xls')
   .helpOption('-h, --help', '查看命令帮助文档');
 
 // 报错时提示通过 -h 或 --help 查看命令帮助文档
@@ -41,6 +39,6 @@ if (!process.argv.slice(2).length) {
 
 const options = program.opts();
 // 当有文件输入时导出文件
-if (options.input) excel2json(options);
+if (options.input) json2excel(options);
 
 // 获取非命令参数 program.args
